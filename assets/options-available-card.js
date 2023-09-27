daa294b15c94773975ac88b36c397ff3bca6a862
Eric Martinez <eric@ericmartinez.dev>
2023-09-27 04:33:35 -0400

    darkened the first swatch border and when slide-btn is clicked, that img stays on active

--- assets/options-available-card.js
+++ assets/options-available-card.js
@@ -2,17 +2,18 @@ const buttons = [...document.getElementsByClassName("slide-btn")]
 
 buttons.forEach(button => {
   button.addEventListener("click", (e) => {
-    console.log(e)
+    button.closest(".product-images").style.opacity = 1
+  
+
     const offset = button.dataset.slideBtn === "next" ? 1 : -1
     const slides = button.closest(".card-carousel").getElementsByClassName("card-carousel__list")[0]
 
     const activeSlide = slides.getElementsByClassName("data-active")[0]
     let newIndex = [...slides.children].indexOf(activeSlide) + offset
-    console.log(newIndex)
+
     if (newIndex < 0) newIndex = slides.children.length - 1
     if (newIndex >= slides.children.length) newIndex = 0
 
-    console.log(slides.children[newIndex])
     slides.children[newIndex].classList.add("data-active")
     activeSlide.classList.remove("data-active")
   })
